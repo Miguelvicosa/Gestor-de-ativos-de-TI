dad9ede7683d3fcd4d100d23b968f55aa819a1ad
Miguel <guerreroxt@gmail.com>
2025-11-19 14:56:56 -0300

    Fases 1 e 2 Concluidas

--- inventario_TI/models.py
+++ inventario_TI/models.py
@@ -19,22 +19,49 @@ class Categoria(models.Model):
 # === Model 2 : Equipamentos ===
 class Equipamento(models.Model):
 
+    nome = models.CharField(
+        max_length=200,
+        verbose_name="Nome do Equipamento"
+    )
+    numero_serie = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Número de Série"
+    )
+    cor = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    data_aquisicao = models.DataField(
+        verbose_name="Data de Aquisição",
+        blank=True,
+        null=True
+    )
     categoria = models.ForeignKey(
         Categoria,
         on_delete=models.SET_NULL,
         null = True,
         blank = True,
         related_name = 'equipamentos',
-        verbose_name = "Categoria"
+        verbose_name = "Categoria",
     )
-    
+    STATUS_CHOICES = (
+        ('EM_USO', 'Em uso'),
+        ('ESTOQUE', 'Estoque'),
+        ('MANUTENCAO', 'Manutenção')
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='ESTOQUE',
+        verbose_name="Status do Equipamento"
+    )
+   
     class Meta:
         verbose_name = "Equipamento"
         verbose_name_plural = "Equipamentos"
-        nome = models.CharField(max_length= 200, verbose_name="Nome do Equipamento")
-        numero_serie = models.CharField(max_length=100, unique=True, verbose_name="Número de Série")
-        cor = models.CharField(max_length=50, blank=True, null=True )
-        data_aquisicao = models.DateField(null=True, blank=True, verbose_name="Data de Aquisição")
+
     
     def __str__(self):
 
